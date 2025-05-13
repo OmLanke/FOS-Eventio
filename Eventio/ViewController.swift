4a9bc5840b132252ce1e0b52a6f9c037f937d43a
Om Lanke <oplanke@gmail.com>
2025-05-13 15:03:22 +0530

    Initial Commit

---
+++ Eventio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eventio
+//
+//  Created by Om Lanke on 13/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
